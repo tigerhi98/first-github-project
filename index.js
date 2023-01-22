fc86ac5e7f14e95eb238392c48576eb91d88ef99
tigerhi98 <118581272+tigerhi98@users.noreply.github.com>
2023-01-22 22:50:17 +0700

    Create index.js
    add feature to set name user-name using localStorage

---
+++ index.js
@@ -0,0 +1,14 @@
+const userNameElem = document.querySelector('user-name')
+setUserName()
+
+function setUserName() {
+  let userName = localStorage.getItem('name')
+  if(userName === undefined) {
+    userName = prompt('What is your name?')
+    if(!name) {
+      setUserName()
+    } else {
+      userNameElem.textContent = userName
+    }
+  }
+}
